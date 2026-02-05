5b12f9b6c381978ce6b068795228e34ad0241826
Ghost <duchojakub987@gmail.com>
2026-02-05 14:03:17 +0100

    form

--- src/main/java/ghost/chess/gui/GUI.java
+++ src/main/java/ghost/chess/gui/GUI.java
@@ -1,11 +1,20 @@
 package ghost.chess.gui;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.paint.Color;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 
 public class GUI {
@@ -20,30 +29,53 @@ public class GUI {
 
     public void createWindow() {
         primaryStage.setTitle("Hello World!");
-        StackPane root = new StackPane();
 
-        root.setStyle("-fx-background-color: coral;");
+        final GridPane grid = createGrid();
 
-        Button btn = createButton(); 
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
 
-        root.getChildren().addAll(btn);
+        Label userName = new Label("username:");
+        grid.add(userName, 0, 1);
+        
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
 
-        primaryStage.setScene(new Scene(root, width, height));
-        primaryStage.show();
-    }
+        Label pw = new Label("password:");
+        grid.add(pw, 0, 2);
 
-    private Button createButton() {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
 
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+            public void handle(ActionEvent e) {
+                actionTarget.setFill(Color.FIREBRICK);
+                actionTarget.setText("Sign in button pressed");
             }
         });
 
-        return btn;
+        final Scene scene = new Scene(grid, width, height);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    private GridPane createGrid() {
+        GridPane grid = new GridPane();       
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        return grid;
+    }
 }
